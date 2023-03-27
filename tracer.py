eb819fbfc1ede9b780c3d0e2d6b8ebc0da4b1fd0
msh1307 <86157260+msh1307@users.noreply.github.com>
2023-03-28 01:25:14 +0900

    Update tracer.py

--- tracer.py
+++ tracer.py
@@ -1,8 +1,9 @@
 import gdb
-enable_syscall_trace = False
+
+enable_syscall_trace = True
 syscalls = ['mmap', 'read', 'write', 'brk', 'open', 'mprotect']
 syscall_str = ' '.join(syscalls)
-sym = ['START_TRACE']
+sym = []
 def stop_handler(stopEvent):
     global sym
     sym_ = gdb.execute("info symbol $rip",to_string=True)
@@ -22,23 +23,27 @@ if 'call ' in gdb.execute("x/xi $rip",to_string=True):
     print("TRACE")
     if enable_syscall_trace:
         gdb.execute(f"catch syscall {syscall_str}")
+    a = gdb.execute("info symbol $rip",to_string=True)
+    if "No symbol" in a:
+        sym.append("START_TRACE")
+    else:
+        sym.append(a[:a.find(' ')])
     gdb.execute("si")
-    gdb.events.stop.connect (stop_handler)
+    gdb.events.stop.connect(stop_handler)
     while str(gdb.parse_and_eval("$rip")) != next:
         gdb.execute("si")
     gdb.events.stop.disconnect(stop_handler)
     c = 0
-    flag = {x : 0 for x in set(sym)}
+    flag = {x : -1 for x in set(sym)}
     for i,j in enumerate(sym):
         if '_dl_runtime_resolve' in j:
-            flag[j]=0
-        if i != 0:
-            if flag[j]==0:
-                flag[j] =c+1
-                c += 1
-            elif flag[j]:
-                c = flag[j]
+            flag[j]=-1
+        if flag[j]==-1:
+            flag[j] =c
+        elif flag[j] != -1:
+            c = flag[j]
         tmp = '  ' * c + j
         print(f"{i : <3} | {tmp}")
+        c += 1
 else:
     print("OPCODE != call")
